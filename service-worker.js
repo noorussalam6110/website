98353e9c66b3ba957d8a54646d0ee1cd5bac1602
noorussalam6110 <noorussalam6110@gmail.com>
2026-09-11 17:03:41 +0530

    Update and rename class5-zone.html to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'noorussalam-v12';
+const urlsToCache = ['./', './index.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names => Promise.all(
+      names.map(name => { if (name !== CACHE_NAME) return caches.delete(name); })
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
